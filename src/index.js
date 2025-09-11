87ac47015684ce8815eee6be3bbb0339ac039b4e
Motoya Nonaka <Motoya.Nonaka@ibm.com>
2025-09-11 13:53:24 +0900

    add fake secret key

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 API_KEY = "ce6fa77263f15f15ff8a889ba3deeb57d6b5232a433f4fe41c75f5c865f19e7b"
+SECRET_KEY = "7fa96cf6e1987fa29569acc71a2377cff0421aace8f15d8f165e06dc162f13f5"
 
 console.log("Hello world");
