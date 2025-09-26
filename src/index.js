5b37135d745fcae120ab2b0f225288fe84116066
Motoya Nonaka <Motoya.Nonaka@ibm.com>
2025-09-26 18:36:10 +0900

    add anti-pattarn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 API_KEY = "ce6fa77263f15f15ff8a889ba3deeb57d6b5232a433f4fe41c75f5c865f19e7b"
 SECRET_KEY = "7fa96cf6e1987fa29569acc71a2377cff0421aace8f15d8f165e06dc162f13f5"
+ADMIN_MAIN_ADDRESS = "admin@example.com"
 
 console.log("Hello world");
+
+// Here is the Non-inclusive language you should NOT use
+// Gendered Language 
+// Instead of: "mailman," "chairman," "guys," "fireman"
+// Use: "mail carrier," "chairperson," "folks" or "everyone," "firefighter"
